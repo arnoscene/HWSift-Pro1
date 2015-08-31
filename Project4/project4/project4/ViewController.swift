4a7d5e6e0ae1825207eef539ba781f41018306a3
Arnold <slowscope@gmail.com>
2015-08-31 11:32:54 +0100

    Project 4
    lots of web like things and creating out buttons from code not from
    storyboard

---
+++ Project4/project4/project4/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  project4
+//
+//  Created by Charnold on 31/08/2015.
+//  Copyright (c) 2015 Arnold. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController,WKNavigationDelegate {
+
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        self.view = webView
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+//        let url = NSURL(string: "http://www.appstorius.com")!
+//        webView.loadRequest(NSURLRequest(URL: url))
+//        webView.allowsBackForwardNavigationGestures = true
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .Plain, target: self, action:"openTapped")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func openTapped(){
+        let ac = UIAlertController(title: "Open page", message: nil, preferredStyle: .ActionSheet)
+        ac.addAction(UIAlertAction(title: "apple.com", style: .Default, handler: openPage))
+            ac.addAction(UIAlertAction(title: "slashdot.org", style: .Default, handler: openPage))
+            ac.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+        presentViewController(ac, animated: true, completion: nil)
+
+    }
+    
+    func openPage(action: UIAlertAction!){
+        
+        let url = NSURL(string: "http://" + action.title)!
+        webView.loadRequest(NSURLRequest(URL: url))
+        
+    }
+
+
+}
+
