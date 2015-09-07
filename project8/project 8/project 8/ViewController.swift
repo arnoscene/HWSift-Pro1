5a0f9983b93d2976e83e7d8a0270bcbcbded94ef
Arnold <slowscope@gmail.com>
2015-09-07 17:50:04 +0100

    project 8
    layout

---
+++ project8/project 8/project 8/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  project 8
+//
+//  Created by Charnold on 07/09/2015.
+//  Copyright (c) 2015 Arnold. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
