9f922840313a88d4eb52e599310afadba61ec027
Arnold <slowscope@gmail.com>
2015-09-09 15:40:33 +0100

    project 10
    having trouble with ui

---
+++ project 10/project10/project10/PersonCell.swift
@@ -0,0 +1,16 @@
+//
+//  PersonCell.swift
+//  project10
+//
+//  Created by Charnold on 09/09/2015.
+//  Copyright (c) 2015 Arnold. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    
+}
---
+++ project 10/project10/project10/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  project10
+//
+//  Created by Charnold on 09/09/2015.
+//  Copyright (c) 2015 Arnold. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: indexPath) as! PersonCell
+        return cell
+    }
+}
+
